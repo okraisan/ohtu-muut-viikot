a7b000164e6f2b09f3c22d66275eb2db69092973
Oona Räisänen <okraisan@protonmail.com>
2019-11-25 18:29:31 +0200

    teht2

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,17 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleLadataanPositiivinenSumma() {
+        kassa.lataa(kortti, 10);
+        verify(kortti, times(1)).lataa(10);
+    }
+    
+    @Test
+    public void kortilleEiLadataNegatiivistaSummaa() {
+        kassa.lataa(kortti, -10);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
